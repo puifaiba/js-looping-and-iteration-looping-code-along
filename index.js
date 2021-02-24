0df69d6c98f03608734331894bea512fb0ec22aa
puifaiba <puifai.ba@gmail.com>
2021-02-24 18:24:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let message_arr = []
+    for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    message_arr.push(message)
+    }
+    return message_arr;
+}
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
